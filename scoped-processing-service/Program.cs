d37ee7ad3ae77d4ac24a1729d46d89f4cc522a3e
dave.han <dave.han@nsuslab.com>
2023-07-14 14:14:12 +0900

    Add Scoped Processing Service

---
+++ scoped-processing-service/Program.cs
@@ -0,0 +1,29 @@
+using scoped_processing_service.Worker;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddHostedService<ConsumeScopedServiceHostedService>();
+builder.Services.AddScoped<IScopedProcessingService, ScopedProcessingService>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
