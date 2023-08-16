2edfa897ea50c9d1645071ab5b29ae4a89815493
dave.han <dave.han@nsuslab.com>
2023-08-16 15:15:38 +0900

    Add HeaderMiddleware

--- logging/Middlewares/HttpHeader/HeaderMiddleware.cs
+++ logging/Middlewares/HttpHeader/HeaderMiddleware.cs
@@ -2,5 +2,17 @@ namespace logging.Middlewares.HttpHeader;
 
 public class HeaderMiddleware
 {
+    private readonly RequestDelegate _next;
+
+    public HeaderMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
     
+    public async Task Invoke(HttpContext context)
+    {
+        context.Request.Headers.Add("X-Correlation-ID", context.Request.Headers["X-Correlation-ID"].FirstOrDefault() ?? Guid.NewGuid().ToString());
+        
+        await _next(context).ConfigureAwait(false);
+    }
 }
